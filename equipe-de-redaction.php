8fd27e915b3e34c012d1da65a06f8994a7b213a4
filyy03 <toure.fily21@yahoo.com>
2025-05-26 11:14:22 +0200

    rien /

--- equipe-de-redaction.php
+++ equipe-de-redaction.php
@@ -22,7 +22,7 @@ require_once('./ressources/includes/connexion-bdd.php');
     <link rel="stylesheet" href="./ressources/css/ne-pas-modifier/global.css">
     <link rel="stylesheet" href="./ressources/css/ne-pas-modifier/header.css">
     <link rel="stylesheet" href="./ressources/css/ne-pas-modifier/accueil.css">
-    <link rel="stylesheet" href="../resssources/css/equipe-de-redaction.css">
+    <link rel="stylesheet" href="resssources/css/equipe-de-redaction.css">
 
 </head>
 
